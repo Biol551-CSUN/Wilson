389a31d3f2cdaca0f7bc3bf54c991a5c2aa3ef1c
Emily Wilson <emwilson243@gmail.com>
2021-04-21 12:27:56 -0700

    Update plot with y axis sorted by latitude

--- Week_12/scripts/week12b_factorslab.R
+++ Week_12/scripts/week12b_factorslab.R
@@ -9,22 +9,24 @@ library(here)
 
 # Load data ################################################################################
 intertidal <- read_csv(here("Week_12", "data", "intertidaldata.csv"))
+int_lat <- read_csv(here("Week_12", "data", "intertidaldata_latitude.csv"))
 
 # Get graphing ##############################################################################
 
 
 intertidal %>% 
+  inner_join(int_lat) %>% 
   mutate(Quadrat = factor(Quadrat)) %>% # make quadrat a factor
   mutate(Site = factor(Site)) %>% # make site a factor, just for the fun of it
   mutate(Quadrat = fct_recode(Quadrat, "Mid" = "Mid  1")) %>% # get rid of weird labels
   mutate(Quadrat = fct_recode(Quadrat, "Low" = "Low  .")) %>% # get rid of weird labels
-  mutate(Quadrat = fct_relevel(Quadrat, levels = c("Low", "Mid", "High"))) %>% # revelel from low to high
+  mutate(Quadrat = fct_relevel(Quadrat, levels = c("Low", "Mid", "High"))) %>% # relevel from low to high
   droplevels() %>% # drop any weird extra leftover levels
-  select(Site, Transect, Quadrat, Algae) %>% 
-  group_by(Site, Quadrat) %>%
+  select(Site, Transect, Latitude, Quadrat, Algae) %>% 
+  group_by(Site, Latitude, Quadrat) %>%
   summarise(avg_alg = mean(Algae)) %>% 
   ggplot(aes(x = Quadrat, # plot quadrat on x axis
-           y = Site, # and site on y axis
+           y = fct_reorder(Site, Latitude), # and site on y axis; reorder by latitude
            fill = avg_alg)) + # fill with algal abundances
   geom_tile() + # make it a tile chart
   coord_equal() + # make the tiles perfect boxes
@@ -32,6 +34,7 @@ intertidal %>%
   theme_minimal() + # nice minimal theme
   labs(title = "Intertidal Algal Abundances", # add plot title
        subtitle = "Average algal abundances across tidal heights", # add subtitle
+       y = "Site",
        fill = "Algae") + # change legend
   theme(plot.title = element_text(hjust = 0.6), # center plot title
         plot.subtitle = element_text(hjust = 0.6)) + 
